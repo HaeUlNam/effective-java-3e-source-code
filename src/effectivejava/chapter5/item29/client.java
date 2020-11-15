d99c52fb23b996165c11cd37291edd275140467b
HaeUlNam <harrynam3@naver.com>
2020-11-15 15:19:03 +0900

    Add usingObject code

--- src/effectivejava/chapter5/item29/client.java
+++ src/effectivejava/chapter5/item29/client.java
@@ -3,8 +3,11 @@ package effectivejava.chapter5.item29;
 public class client {
 
   public static void main(String[] args) {
-    
+    StackUsingObject stackUsingObject = new StackUsingObject();
+    stackUsingObject.push(1);
+    stackUsingObject.push("abcString");
 
+    Integer a = (Integer) stackUsingObject.pop();
   }
 
 }
