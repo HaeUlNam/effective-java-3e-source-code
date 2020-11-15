0ae46614d3c9b9bef5c4914815347a462f26b9b8
HaeUlNam <harrynam3@naver.com>
2020-11-15 15:21:40 +0900

    refactor code

--- src/effectivejava/chapter5/item29/client.java
+++ src/effectivejava/chapter5/item29/client.java
@@ -3,11 +3,20 @@ package effectivejava.chapter5.item29;
 public class client {
 
   public static void main(String[] args) {
+    stackUsingObjectCode();
+    
+  }
+
+
+  /**
+   * 스택을 Object로 만들었을 때 ClassCastException 발생
+   */
+  public static void stackUsingObjectCode() {
     StackUsingObject stackUsingObject = new StackUsingObject();
     stackUsingObject.push(1);
     stackUsingObject.push("abcString");
 
-    Integer a = (Integer) stackUsingObject.pop();
+    Integer a = (Integer) stackUsingObject.pop(); // 에러 나는 부분
   }
 
 }
